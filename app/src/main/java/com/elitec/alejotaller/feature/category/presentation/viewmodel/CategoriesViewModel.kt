f8e652b0f9d525d6cd0f6e9e3bedcc609db272f0
Danielito1996 <daniel.imbert96@hotmail.com>
2026-02-07 16:06:33 -0600

    offline first config

--- app/src/main/java/com/elitec/alejotaller/feature/category/presentation/viewmodel/CategoriesViewModel.kt
+++ app/src/main/java/com/elitec/alejotaller/feature/category/presentation/viewmodel/CategoriesViewModel.kt
@@ -4,23 +4,20 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.elitec.alejotaller.feature.category.domain.caseUse.ObserveCategoriesCaseUse
 import com.elitec.alejotaller.feature.category.domain.caseUse.SyncCategoriesCaseUse
-import com.elitec.alejotaller.feature.category.domain.entity.Category
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class CategoriesViewModel(
-    private val observeCategories: ObserveCategoriesCaseUse,
+    observeCategories: ObserveCategoriesCaseUse,
     private val syncCategories: SyncCategoriesCaseUse
 ): ViewModel() {
-    val _categoriesFlow = observeCategories()
+    val categoriesFlow = observeCategories()
         .stateIn(
             viewModelScope, SharingStarted.WhileSubscribed(5_000),
             emptyList()
         )
-    val categoriesFlow get() = _categoriesFlow
+
 
     init {
         viewModelScope.launch {
